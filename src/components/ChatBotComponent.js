fcb4834cab1d0024b10eb692e6edda9a52adc145
KemoLD <kemo.sonko1@gmail.com>
2023-10-21 23:15:23 -0700

    fixed ui

--- src/components/ChatBotComponent.js
+++ src/components/ChatBotComponent.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 const ChatBotComponent = () => {
   const iframeStyle = {
-    minWidth: '400px',
-    width: '100%',
-    minHeight: '500px',
+    minWidth: '400px', width: '100%', minHeight: '500px', border: 'none'
   };
 
   return (
-    <iframe src='https://webchat.botframework.com/embed/demo-name-bot1?s=IHxwTDT_KqU.I2LhDVgl5h9G5ArqS3AVN5Qqsxt6qasw85OumiIeLlo'  style={iframeStyle} title='RenewU Chat Bot' ></iframe>
+    <iframe src='https://webchat.botframework.com/embed/demo-name-bot1?s=IHxwTDT_KqU.I2LhDVgl5h9G5ArqS3AVN5Qqsxt6qasw85OumiIeLlo' style={iframeStyle} title='RenewU Chat Bot' ></iframe>
   );
 };
 
--- src/pages/ChatBot.js
+++ src/pages/ChatBot.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Typography } from '@mui/material';
 import ChatBotComponent from '../components/ChatBotComponent';
+import { Typography } from '@mui/material';
 
 const ChatBot = () => {
-
   return (
     <div
       style={{
@@ -32,21 +31,32 @@ const ChatBot = () => {
         RenewU
       </Typography>
 
-      <Typography
-        variant="body1"
-        sx={{
-          textAlign: 'center',
-          color: '#333',
-          lineHeight: '1.5',
-          padding: '20px 0', // add padding to the top and bottom
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
         }}
       >
-        Our chatbot is here to assist you on your journey to personal growth and self-improvement. It's easy to get started. Simply click on the chat window below and type your questions or topics you'd like to discuss. Our chatbot is trained on the wisdom of the best lifestyle and business books to provide you with valuable insights and motivation.
-        Whether you need guidance on setting goals, building habits, or just seeking daily inspiration, RenewU is here for you. Don't hesitate to start a conversation with our chatbot, and let's embark on your journey to becoming the best version of yourself, one conversation at a time.
-        Enter a prompt into the chat window below to get started and take the first step towards personal growth.
-      </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            textAlign: 'center',
+            color: '#333',
+            lineHeight: '1.5',
+            padding: '20px 0', // add padding to the top and bottom
+          }}
+        >
+          Our chatbot is here to assist you on your journey to personal growth and self-improvement. It's easy to get started. Simply click on the chat window below and type your questions or topics you'd like to discuss. Our chatbot is trained on the wisdom of the best lifestyle and business books to provide you with valuable insights and motivation.
+          Whether you need guidance on setting goals, building habits, or just seeking daily inspiration, RenewU is here for you. Don't hesitate to start a conversation with our chatbot, and let's embark on your journey to becoming the best version of yourself, one conversation at a time.
+          Enter a prompt into the chat window below to get started and take the first step towards personal growth.
+        </Typography>
 
-      <ChatBotComponent />
+        <div style={{ flex: 1 }}>
+          <ChatBotComponent style={{ height: '100%' }} />
+        </div>
+
+      </div>
     </div>
   );
 };
